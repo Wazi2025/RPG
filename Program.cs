b6f4ce6dee7b40af48077def690271cc38009c82
Jan Leirvaag <lja022025@gmail.com>
2025-03-12 14:22:32 +0100

    new

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int heroHealth = 10;
+int monsterHealth = 10;
+int damage = 0;
+
+Random dice = new Random();
+
+while (heroHealth > 0 || monsterHealth > 0)
+{
+    damage = dice.Next(1, 10);
+    monsterHealth -= damage;
+}
+
+Console.WriteLine($"Monster's health: {monsterHealth}");
+
+Console.WriteLine($"Monster's health: {heroHealth}");
